7c051a7eb8c243f8c7ec664af6bf663e23bdcd73
TheCoreExt <diegorock12@icloud.com>
2017-04-28 01:05:09 -0500

    .

--- Proyecto2/proyecto2.c
+++ Proyecto2/proyecto2.c
@@ -70,7 +70,7 @@ int main(int argc, char *argv[]) {
             printf("Ingrese el valor maximo: ");
             scanf("%f", &max);
           break;
-
+//
         default:
             break;
     }
